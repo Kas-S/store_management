b9b38e1f477112ffde85eddd98de635a7a4f9aff
Kas S <sotimkul@gmail.com>
2024-06-12 22:33:47 +0500

    Basic routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-function App() {
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {Layout, Home, Login} from "./pages"
 
+function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>} >
+          <Route index element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+import Layout from "./Layout.tsx"
+import Home from "./Home.tsx"
+import Login from "./Login.tsx"
+
+export {Layout, Home, Login}
