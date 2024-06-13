6d9e690b7e4b4c9a755e50af720f70ca8ef71c53
Kas S <sotimkul@gmail.com>
2024-06-13 15:08:15 +0500

    ChakraUI provider added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {ChakraProvider} from "@chakra-ui/react"
 import {Layout, Home, Login} from "./pages"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>} >
-          <Route index element={<Home/>}/>
-          <Route path="/login" element={<Login/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>} >
+                    <Route index element={<Home/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </ChakraProvider>
   )
 }
 
