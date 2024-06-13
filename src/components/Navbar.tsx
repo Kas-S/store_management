dbaadb33c20aa95daaa7d079540f3cd2f5b1ab06
Kas S <sotimkul@gmail.com>
2024-06-13 15:19:45 +0500

    Navbar layout is done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,16 @@ import {Menu} from "./"
 
 function Navbar() {
     return (
-        <nav className="fixed top-0 left-0 z-50 bg-gray-100 w-full pt-5 pb-5 pr-6 pl-6 shadow-lg flex justify-between items-center">
-            <Link to="/" className="font-bold text-4xl">SM</Link>
-            <Menu/>
-        </nav>
+        <>
+            <nav className="fixed top-0 left-0 z-50 bg-gray-100 w-full pt-5 pb-5 pr-6 pl-6 shadow-lg flex justify-between items-center">
+                <Link to="/" className="font-bold text-4xl">SM</Link>
+                <Menu/>
+            </nav>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        </>
     )
 }
 
