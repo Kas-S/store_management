8d8f88d799c73e11a03b22d29bbbb563d69fedb6
Kas S <sotimkul@gmail.com>
2024-06-16 22:29:37 +0500

    Unauthorized users redirecting optimized

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,16 @@
-import {Outlet} from "react-router-dom"
+import {Outlet, useNavigate} from "react-router-dom"
 import {Navbar} from "../components"
+import {useContext, useEffect} from "react";
+import {UserContext} from "../UserContext.ts";
 
 function Layout() {
+    const user = useContext(UserContext),
+        navigate = useNavigate()
+    useEffect(() => {
+        if (!user) {
+            navigate("/login")
+        }
+    }, [user]);
     return (
         <>
             <Navbar/>
