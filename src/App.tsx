692cc93ac0fdef1f2bcb750f1275005b8275330f
Kas S <sotimkul@gmail.com>
2024-06-16 22:18:47 +0500

    User context added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
+import {useState, useEffect} from "react"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import {ChakraProvider} from "@chakra-ui/react"
+import {onAuthStateChanged} from "firebase/auth"
 import {Layout, Home, Login} from "./pages"
+import {auth} from "./firebase.ts"
+import {UserContext} from "./UserContext.ts"
+import {User} from "firebase/auth"
 
 function App() {
+  const [user, setUser] = useState<User | null>(null)
+  useEffect(() => {
+      onAuthStateChanged(auth, (u) => {
+          setUser(u)
+      })
+  }, [])
   return (
-    <ChakraProvider>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout/>} >
-                    <Route index element={<Home/>}/>
-                </Route>
-                <Route path="/login" element={<Login/>}/>
-            </Routes>
-        </BrowserRouter>
-    </ChakraProvider>
+    <UserContext.Provider value={user}>
+        <ChakraProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout/>} >
+                        <Route index element={<Home/>}/>
+                    </Route>
+                    <Route path="/login" element={<Login/>}/>
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
+    </UserContext.Provider>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,17 @@
+import {useEffect, useContext} from "react"
+import {useNavigate} from "react-router-dom"
+import {UserContext} from "../UserContext.ts"
+
 function Home() {
+    const user = useContext(UserContext),
+          navigate = useNavigate()
+    useEffect(() => {
+        if (!user) {
+            navigate("/login")
+        }
+    }, [user]);
     return (
-        <></>
+        <h1 className="text-9xl">Hello World</h1>
     )
 }
 
