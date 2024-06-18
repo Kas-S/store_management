f1961cd5452a034c91405dd9895d89d8a0f3e2eb
Kas S <sotimkul@gmail.com>
2024-06-18 19:03:45 +0500

    Logout function added

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,4 +1,11 @@
+import {useEffect} from 'react'
+import {signOut} from 'firebase/auth'
+import {auth} from '../firebase.ts'
+
 function Logout() {
+    useEffect(() => {
+        signOut(auth)
+    }, []);
     return <></>
 }
 
