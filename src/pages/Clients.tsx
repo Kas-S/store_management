145e88dba0b6e6925428aa4df55ac965e8dacfe3
Kas S <sotimkul@gmail.com>
2024-06-19 22:32:11 +0500

    Clients rendering restructured and delete clients function added

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,38 +1,46 @@
 import {useEffect, useState} from "react"
 import {Link} from "react-router-dom"
-import {getDocs, collection} from "firebase/firestore"
+import {getDocs, collection, deleteDoc, doc} from "firebase/firestore"
 import {db} from "../firebase.ts"
 import {Client} from "../types.ts"
 
 function Clients() {
-    const [renderedClients, setRenderedClients] = useState<JSX.Element[]>([])
+    const [clients, setClients] = useState<Client[]>([])
+    const deleteClient = async(cid: string) => {
+        await deleteDoc(doc(db, `clients/${cid}`)).then(() => {
+            const id = clients.findIndex((c) => (c.cid === cid))
+            setClients(clients.slice(0, id).concat(clients.slice(id + 1)))
+        })
+    }
+    const renderClients = (clients: Client[]) => {
+        return clients.map((client) => {
+            const buttons = [
+                ['Детали', '/clients/' + client.cid, "bg-emerald-600"],
+                ['Изменить', '/clients/edit/' + client.cid, "bg-amber-500"],
+            ]
+            return (
+                <tr className="pt-4 text-xl" key={client.cid}>
+                    <td className="text-center pt-4 pb-4 border-2 border-solid">{client.name}</td>
+                    <td className="text-center border-2 border-solid">{client.contact}</td>
+                    <td className="border-2 border-solid text-center text-lg">
+                        {buttons.map((button) => (
+                            <Link key={button[0]} to={button[1]} className={"p-3 rounded text-white mr-1 font-bold " + button[2]}>{button[0]}</Link>
+                        ))}
+                        <button type="button" className="p-3 rounded text-white mr-1 font-bold bg-red-500" onClick={() => deleteClient(client.cid)}>Удалить</button>
+                    </td>
+                </tr>
+            )
+        })
+    }
     useEffect(() => {
         document.title = "Клиенты"
         getDocs(collection(db, "clients"))
             .then((res) => {
-                const clients: Client[] = []
+                const c: Client[] = []
                 res.forEach((doc) => {
-                    clients.push(doc.data() as Client)
+                    c.push(doc.data() as Client)
                 })
-                setRenderedClients(clients.map((client) => {
-                    const buttons = [
-                        ['Детали', '/clients/' + client.cid, "bg-emerald-600"],
-                        ['Изменить', '/clients/edit/' + client.cid, "bg-amber-500"],
-                        ['Удалить', '/clients/delete/' + client.cid, "bg-red-600"]
-                    ]
-                    return (
-                        <tr className="pt-4 text-xl" key={client.cid}>
-                            <td className="text-center pt-4 pb-4 border-2 border-solid">{client.name}</td>
-                            <td className="text-center border-2 border-solid">{client.contact}</td>
-                            <td className="border-2 border-solid text-center text-lg">
-                                {buttons.map((button) => (
-                                    <Link key={button[0]} to={button[1]} className={"p-3 rounded text-white mr-1 font-bold " + button[2]}>{button[0]}</Link>
-                                ))}
-                            </td>
-                        </tr>
-                    )
-                }))
-
+                setClients(c)
             })
     }, [])
     return (
@@ -50,7 +58,7 @@ function Clients() {
                     </tr>
                 </thead>
                 <tbody>
-                    {renderedClients}
+                    {renderClients(clients)}
                 </tbody>
             </table>
         </main>
