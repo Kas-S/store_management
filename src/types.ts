f108d8bb5641a2a52330f6165f211b04a44698d2
Kas S <sotimkul@gmail.com>
2024-06-24 20:00:15 +0500

    Types update

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,24 @@
+import {Dispatch, SetStateAction} from "react";
+
 export type Client = {
     name: string,
     contact: string,
     cid: string,
     admin: string,
     regDate: string
+}
+
+export type formSelect = {
+    title: string,
+    name: string,
+    options: string[],
+    values: string[],
+    handler: Dispatch<SetStateAction<string>>
+}
+
+export type formInput = {
+    title: string,
+    name: string,
+    type: string,
+    handler: Dispatch<SetStateAction<string>>
 }
