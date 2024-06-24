90774971f5a744045dca465a06e6fb0162625d85
Kas S <sotimkul@gmail.com>
2024-06-24 20:04:50 +0500

    Logic is being imported from custom hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+import useFetchClients from "./useFetchClients.ts"
+import useDivideClientsByNameAndID from "./useDivideClientsByNameAndID.ts"
+
+export {useFetchClients, useDivideClientsByNameAndID}
