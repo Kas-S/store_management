2f928115565d9a88020952ae1ca017ebb2b91b9e
Kas S <sotimkul@gmail.com>
2024-06-24 11:30:05 +0500

    Form was recreated with FormInput component

--- src/pages/AddClient.tsx
+++ src/pages/AddClient.tsx
@@ -3,6 +3,7 @@ import {useNavigate} from "react-router-dom"
 import {setDoc, doc} from "firebase/firestore"
 import {db} from "../firebase.ts"
 import {UserContext} from "../UserContext.ts"
+import {FormInput} from "../components"
 
 function AddClient() {
     const [name, setName] = useState<string>(''),
@@ -30,14 +31,8 @@ function AddClient() {
     return (
         <main>
             <form className="flex flex-col items-center justify-center w-full text-center gap-4 font-bold" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="client_name" className="text-xl">Имя клиента: </label><br/>
-                    <input type="text" name="client_name" id="client_name" placeholder="Имя клиента" className="xs:w-[50vw] h-10 rounded border border-black px-3" onChange={(e) => setName(e.target.value)}/>
-                </div>
-                <div>
-                    <label htmlFor="client_contact" className="text-xl">Контакт клиента(Номер телефона): </label><br/>
-                    <input type="text" name="client_contact" id="client_contact" placeholder="Контакт клиента" className="xs:w-[50vw] h-10 rounded border border-black px-3" onChange={(e) => setContact(e.target.value)}/>
-                </div>
+                <FormInput name="client_name" type="text" title="Имя клиента: " handler={setName}/>
+                <FormInput name="client_contact" type="text" title="Контакт клиента(Номер телефона): " handler={setContact}/>
                 <button type="submit" className="p-3 rounded bg-blue-400 text-white font-bold">Зарегистрировать</button>
             </form>
         </main>
