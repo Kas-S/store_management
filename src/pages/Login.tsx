87578f795feb1d2ac429b35d62b4d7c97083a5e7
Kas S <sotimkul@gmail.com>
2024-06-24 11:29:33 +0500

    Login form was recreated with FormInput component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,7 @@ import {signInWithEmailAndPassword} from "firebase/auth"
 import {useState, FormEvent, useEffect} from "react"
 import {useNavigate} from "react-router-dom"
 import {auth} from "../firebase.ts"
+import {FormInput} from "../components"
 
 function Login() {
     useEffect(() => {
@@ -24,18 +25,8 @@ function Login() {
     return (
         <main>
             <form onSubmit={signIn} className="flex flex-col items-center justify-center w-full pt-10 text-center gap-4">
-                <div>
-                    <label htmlFor="login" className="text-2xl font-bold">Логин: </label><br/>
-                    <input type="login" name="login" id="login" onChange={(e) => setLogin(e.target.value)}
-                           className="xs:w-[50vw] md:w-[30vw] h-10 rounded border border-black px-3"
-                           placeholder="Логин"/>
-                </div>
-                <div>
-                    <label htmlFor="password" className="text-2xl font-bold">Пароль: </label><br/>
-                    <input type="password" name="password" id="password" onChange={(e) => setPassword(e.target.value)}
-                           className="xs:w-[50vw] md:w-[30vw] h-10 rounded border border-black px-3"
-                           placeholder="Пароль"/>
-                </div>
+                <FormInput name="login" type="login" title="Логин: " handler={setLogin}/>
+                <FormInput name="password" type="password" title="Пароль: " handler={setPassword}/>
                 <button type="submit" className="border-black p-3 font-bold border rounded hover:bg-black hover:text-white transition-all ease-linear">Войти</button>
             </form>
         </main>
