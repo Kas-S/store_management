a281c764255fa2248969ae6317d698aac1c4973e
Kas S <sotimkul@gmail.com>
2024-06-25 20:28:57 +0500

    Styling update

--- src/pages/Sell.tsx
+++ src/pages/Sell.tsx
@@ -7,7 +7,7 @@ function Sell() {
     const [client, setClient] = useState<string>("")
     console.log(client) // to fix production
     return (
-        <form className="flex items-center flex-col">
+        <form className="flex flex-col items-center justify-center w-full pt-10 text-center gap-4">
             <h1>Продажа</h1>
             <FormSelect name="client" title="Клиент: " options={clientNames} values={clientIDs} handler={setClient}/>
         </form>
