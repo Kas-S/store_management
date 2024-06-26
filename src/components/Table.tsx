b4c2b07107b0f106e2e0e016ffac47aa2343bb81
Kas S <sotimkul@gmail.com>
2024-06-26 19:27:43 +0500

    Minor fix + styling update

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,16 +1,17 @@
 import {PropsWithChildren} from "react"
 
 type tableProps = PropsWithChildren<{
-    fields: string[]
+    fields: string[],
+    cell_width: string
 }>
 
 function Table(props: tableProps) {
     return (
-        <table className="w-full border-2 mt-4 border-solid">
+        <table className="w-full border-2 mt-4 border-solid text-center">
             <thead>
                 <tr>
                     {props.fields.map(field => (
-                        <th className={`pt-4 border-2 border-solid w-1/${props.fields.length}`}>{field}</th>
+                        <th className={"pt-4 border-2 border-solid " + props.cell_width} key={field}>{field}</th>
                     ))}
                 </tr>
             </thead>
