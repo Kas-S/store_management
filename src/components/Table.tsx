738fcb26db0e63526a96bd5143a5914826355ee5
Kas S <sotimkul@gmail.com>
2024-06-26 19:31:34 +0500

    To fix production

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,11 +1,11 @@
 import {PropsWithChildren} from "react"
 
-type tableProps = PropsWithChildren<{
+type tableProps = {
     fields: string[],
     cell_width: string
-}>
+}
 
-function Table(props: tableProps) {
+function Table(props: PropsWithChildren<tableProps>) {
     return (
         <table className="w-full border-2 mt-4 border-solid text-center">
             <thead>
